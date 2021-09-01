6f19eac000a7ccc2d8f83d5631749937977dedbc
Jonas Ohlsson <jonas@vansinne.se>
2021-09-01 18:58:10 +0200

    add long description and classifiers

--- setup.py
+++ setup.py
@@ -3,13 +3,23 @@ from setuptools import setup, find_packages
 with open('LICENSE') as f:
     license = f.read()
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup(
     name='etalumacontrol',
     version='0.1.0',
     description='Library for interacting with Etaluma microscopes and stages',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author='Jonas Ohlsson',
     author_email='jonas.ohlsson@slu.se',
     url='https://github.com/jonasoh/etalumacontrol',
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Scientific/Engineering",
+    ],
     license=license,
     packages=find_packages(),
     include_package_data=True,
